023dbbca173b7f145e98c02f31ebd09ce6c37548
USAMI Kenta <tadsan@zonu.me>
2020-02-05 01:45:07 +0900

    Add /phpinfo.php

--- app/routes.php
+++ app/routes.php
@@ -8,6 +8,9 @@ use Aura\Router\RouterContainer;
 use Psr\Http\Message\ResponseInterface as HttpResponse;
 use Psr\Http\Message\ResponseFactoryInterface as ResponseFactory;
 use Psr\Http\Message\StreamFactoryInterface as StreamFactory;
+use function ob_start;
+use function ob_get_clean;
+use function phpinfo;
 
 $router = new RouterContainer();
 $map = $router->getMap();
@@ -18,5 +21,12 @@ $map->get('index', '/', fn(
     View\HtmlFactory $html
 ): HttpResponse => $factory->createResponse()->withBody($stream->createStream($html('index', [
 ]))));
+
+$map->get('phpinfo', '/phpinfo.php', function (ResponseFactory $factory, StreamFactory $stream) {
+    ob_start();
+    phpinfo();
+
+    return $factory->createResponse()->withBody($stream->createStream(ob_get_clean()));
+});
 
 return $router;
