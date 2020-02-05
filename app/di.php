ecd19bd679ac549a5d7fa70fc2c6d25fd0d76d61
USAMI Kenta <tadsan@zonu.me>
2020-02-06 00:37:39 +0900

    Add JWT and UuidFactory to di.php

--- app/di.php
+++ app/di.php
@@ -7,13 +7,16 @@ namespace Playground\Web;
 use Aura\Router\RouterContainer;
 use Cake\Chronos\Chronos;
 use DI;
+use Firebase\JWT\JWT;
 use Laminas\HttpHandlerRunner\Emitter\EmitterInterface;
 use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
 use Nyholm\Psr7Server\ServerRequestCreator;
 use Nyholm\Psr7\Factory\Psr17Factory;
-use Playground\Web\View\HtmlFactory;
+use Playground\Web\View;
 use Psr\Container\ContainerInterface as Container;
 use Psr\Http\Message\{RequestFactoryInterface, RequestInterface, ResponseFactoryInterface, ResponseInterface, ServerRequestFactoryInterface, ServerRequestInterface, StreamFactoryInterface, StreamInterface, UploadedFileFactoryInterface, UploadedFileInterface, UriFactoryInterface, UriInterface};
+use Ramsey\Uuid\UuidFactory;
+use Ramsey\Uuid\UuidFactoryInterface;
 use Twig\Environment as Twig;
 use Twig\Extension\OptimizerExtension as TwigOptimizer;
 use Twig\Loader\FilesystemLoader as TwigFilesystemLoader;
@@ -33,6 +36,10 @@ $builder = new DI\ContainerBuilder();
 $builder->addDefinitions((include __DIR__ . '/../config.php') + [
     Chronos::class => create(Chronos::class),
     EmitterInterface::class => create(SapiEmitter::class),
+    JWT::class => create(JWT::class),
+    JwtEncoder::class => factory(function (JWT $jwt) {
+        return new JwtEncoder($jwt);
+    }),
     Psr17Factory::class => create(Psr17Factory::class),
     RequestFactoryInterface::class => get(Psr17Factory::class),
     ResponseFactoryInterface::class => get(Psr17Factory::class),
@@ -71,14 +78,15 @@ $builder->addDefinitions((include __DIR__ . '/../config.php') + [
     }),
     UploadedFileFactoryInterface::class => get(Psr17Factory::class),
     UriFactoryInterface::class => get(Psr17Factory::class),
+    UuidFactoryInterface::class => get(UuidFactory::class),
     WhoopsInterface::class => factory(function () {
         $whoops = new Whoops;
         $whoops->appendHandler(new PrettyPageHandler());
 
         return $whoops;
     }),
-    HtmlFactory::class => factory(function (Twig $twig) {
-        return new HtmlFactory($twig);
+    View\HtmlFactory::class => factory(function (Twig $twig) {
+        return new View\HtmlFactory($twig);
     }),
 ]);
 
